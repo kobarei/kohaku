e2d41a5b4ab7c394b6155b8cc7838c93ab3ebc47
NAKAI Ryosuke <nakai@shiguredo.jp>
2021-09-02 10:21:10 +0900

    main.go が抜けていた

---
+++ cmd/kohaku/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/shiguredo/kohaku"
+
+	zlog "github.com/rs/zerolog/log"
+
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+)
+
+// curl -v --http2-prior-knowledge http://localhost:8080
+
+// TODO(v): 特定の IP アドレスからしか受け付けないようにする
+// TODO(v): 性能測定
+// TODO(v): YAML 化
+
+func init() {
+	// 設定の読み込みと、Logger の準備
+	flag.Parse()
+	// load config
+	if err := kohaku.LoadConfigFromFlags(kohaku.ConfigFilePath); err != nil {
+		log.Fatalf("config file read error: %s", err)
+	}
+	err := kohaku.InitLogger(kohaku.Config.LogDir, kohaku.Config.LogName, kohaku.Config.Debug, kohaku.Config.LogStdout)
+	if err != nil {
+		log.Fatalf("logger building failed. %s", err)
+	}
+	zlog.Info().
+		Msg("FinishInitProcess")
+}
+
+func pgxPoolMiddleware(pool *pgxpool.Pool) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Set("pool", pool)
+		c.Next()
+	}
+}
+
+func main() {
+	r := gin.New()
+
+	// TODO(v): YAML で外に出す
+	var connStr = "postgres://postgres:password@127.0.0.1:5432/kohaku?sslmode=disable"
+	pool, err := pgxpool.Connect(context.Background(), connStr)
+	if err != nil {
+		// TODO(v): エラーメッセージを修正する
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer pool.Close()
+
+	if err := pool.Ping(context.Background()); err != nil {
+		// TODO(v): エラーメッセージを修正する
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+
+	// TODO(v): カスタムコンテキストに Pool を渡すかたちでいいのかどうか確認する
+	r.Use(pgxPoolMiddleware(pool))
+
+	// TODO(v): zerolog に切り替える
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+
+	// 統計情報を突っ込むところ
+	r.POST("/collector", kohaku.Collector)
+
+	// TODO(v): ヘルスチェック用の /status みたいなのあった方がいい
+	// TODO(v): こいつ自身の統計情報を /stats でとれた方がいい
+
+	// TODO(v): HTTP/2 の設定は YAML へ
+	h2s := &http2.Server{
+		MaxConcurrentStreams: 250,
+		MaxReadFrameSize:     1048576,
+		IdleTimeout:          600 * time.Second,
+	}
+
+	s := http.Server{
+		Addr: fmt.Sprintf(":%d", kohaku.Config.CollectorPort),
+		// TODO(v): YAML で h2c と h2 を切り替えられるようにする
+		Handler: h2c.NewHandler(r, h2s),
+	}
+
+	// &tls.Config{NextProtos: []string{"h2"}}
+	// s.ListenAndServeTls("crt", "key")
+	if err := s.ListenAndServe(); err != http.ErrServerClosed {
+		log.Fatal(err)
+	}
+}
