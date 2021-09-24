cfdbbb11606f9c7a998ce027040903bc8ca5f3f6
Yoshida Hiroshi <yoshida@shiguredo.jp>
2021-09-24 16:44:55 +0900

    binding に修正する

--- sora.go
+++ sora.go
@@ -8,22 +8,22 @@ import (
 // type は PeerConnection / SoraConnection
 // type: connection.remote / type: connection.sora
 type SoraStatsExporter struct {
-	Type string `json:"type" validate:"required"`
+	Type string `json:"type" binding:"required"`
 
 	Label     string    `json:"label"`
 	Version   string    `json:"version"`
-	Timestamp time.Time `json:"timestamp" validate:"required"`
+	Timestamp time.Time `json:"timestamp" binding:"required"`
 
-	Role string `json:"role" validate:"required"`
+	Role string `json:"role" binding:"required"`
 
-	ChannelID    string `json:"channel_id" validate:"required"`
-	ClientID     string `json:"client_id" validate:"required"`
-	ConnectionID string `json:"connection_id" validate:"required"`
+	ChannelID    string `json:"channel_id" binding:"required"`
+	ClientID     string `json:"client_id" binding:"required"`
+	ConnectionID string `json:"connection_id" binding:"required"`
 
 	// TODO(v): required にする
 	Multistream bool `json:"multistream"`
 	Simulcast   bool `json:"simulcast"`
 	Spotlight   bool `json:"spotlight"`
 
-	Stats []json.RawMessage `json:"stats" validate:"required"`
+	Stats []json.RawMessage `json:"stats" binding:"required"`
 }
