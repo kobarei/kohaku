266b969c3ae34762d896109c7db207d9016c0518
Yoshida Hiroshi <yoshida@shiguredo.jp>
2021-12-23 14:44:17 +0900

    /status のテストを追加する

---
+++ status_test.go
@@ -0,0 +1,24 @@
+package kohaku
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStatus(t *testing.T) {
+	// Setup
+	req := httptest.NewRequest(http.MethodPost, "/status", strings.NewReader(""))
+	req.Proto = "HTTP/2.0"
+	rec := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(rec)
+	c.Request = req
+
+	// Assertions
+	server.Status(c)
+	assert.Equal(t, http.StatusNoContent, c.Writer.Status())
+}
