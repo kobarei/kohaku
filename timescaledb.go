3fc1702e28583dc97c6f7d9eb4a6db3eb4025811
Yoshida Hiroshi <yoshida@shiguredo.jp>
2021-12-28 16:56:45 +0900

    使用していない構造体をコメントアウトする

--- timescaledb.go
+++ timescaledb.go
@@ -3,28 +3,28 @@ package kohaku
 import "time"
 
 // 使ってない
-type soraNode struct {
-	Timestamp time.Time `db:"timestamp"`
-
-	Label    string `db:"label"`
-	Version  string `db:"version"`
-	NodeName string `db:"node_name"`
-}
+// type soraNode struct {
+// 	Timestamp time.Time `db:"timestamp"`
+//
+// 	Label    string `db:"label"`
+// 	Version  string `db:"version"`
+// 	NodeName string `db:"node_name"`
+// }
 
 // 使ってない
-type soraConnection struct {
-	soraNode
-
-	Multistream bool `db:"multistream"`
-	Simulcast   bool `db:"simulcast"`
-	Spotlight   bool `db:"spotlight"`
-
-	Role         string `db:"role"`
-	ChannelID    string `db:"channel_id"`
-	SessionID    string `db:"session_id"`
-	ClientID     string `db:"client_id"`
-	ConnectionID string `db:"connection_id"`
-}
+// type soraConnection struct {
+// 	soraNode
+//
+// 	Multistream bool `db:"multistream"`
+// 	Simulcast   bool `db:"simulcast"`
+// 	Spotlight   bool `db:"spotlight"`
+//
+// 	Role         string `db:"role"`
+// 	ChannelID    string `db:"channel_id"`
+// 	SessionID    string `db:"session_id"`
+// 	ClientID     string `db:"client_id"`
+// 	ConnectionID string `db:"connection_id"`
+// }
 
 type rtc struct {
 	Time *time.Time `db:"time"`
