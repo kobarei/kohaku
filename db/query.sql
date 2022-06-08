4d4a915e9377f396c5053b88027030c0fe96225e
NAKAI Ryosuke <nakai@shiguredo.jp>
2022-06-09 02:01:50 +0900

    erlang vm 関連を削除

--- db/query.sql
+++ db/query.sql
@@ -1,22 +1,3 @@
--- name: InsertSoraNode :exec
-INSERT INTO sora_node (
-  timestamp,
-  label, version, node_name
-)
-SELECT
-  @timestamp,
-  @label, @version, @node_name
-WHERE
-  NOT EXISTS (
-    SELECT id
-    FROM sora_node
-    WHERE (
-      (label = @label::varchar(255)) AND
-      (version = @version::varchar(255)) AND
-      (node_name = @node_name::varchar(255))
-    )
-);
-
 -- name: InsertSoraConnection :exec
 INSERT INTO sora_connection (
   timestamp,
@@ -39,19 +20,4 @@ WHERE
       (client_id = @client_id::varchar(255)) AND
       (connection_id = @connection_id::char(26))
     )
-);
-
--- name: InsertErlangVMMemoryStats :exec
-INSERT INTO erlang_vm_memory_stats (
-  time,
-  sora_version, sora_label, sora_node_name,
-  stats_type,
-  type_total, type_processes, type_processes_used, type_system,
-  type_atom, type_atom_used, type_binary, type_code, type_ets
-) VALUES (
-  @time,
-  @sora_version, @sora_label, @sora_node_name,
-  @stats_type,
-  @type_total, @type_processes, @type_processes_used, @type_system,
-  @type_atom, @type_atom_used, @type_binary, @type_code, @type_ets
 );
